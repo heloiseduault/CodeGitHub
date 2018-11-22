2b166d154aebfca1448e5815af3cf78bdf473daf
Héloïse DUAULT <heloiseduault@Heloises-MacBook-Pro.local>
2018-11-22 18:11:49 +0100

    hi

--- annoy.js
+++ annoy.js
@@ -1,1 +1,12 @@
-alert("CONNECTED");
+var answer = prompt("are we there yet?");
+
+while(answer !== "yes") {
+    var answer = prompt("are we there yet?");
+}
+
+alert("YAY, WE MADE IT!!");
+
+if(answer === "yes") {
+    alert("YAY, we made it");
+} else {
+}
